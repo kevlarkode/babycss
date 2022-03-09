d53f3398ef9b87f86e4d8f10b97f684149e36eec
Tulsi Dey <deytulsi18@gmail.com>
2022-03-09 21:31:16 +0530

    added navbar and scroll to top btn

--- script.js
+++ script.js
@@ -30,3 +30,65 @@ const toggleItem = (item) => {
         item.classList.add('accordion-open')
     }
 }
+
+/******************* Scoll  To Top *******************/
+window.addEventListener('scroll', () => {
+    let scroll = document.querySelector('.scrollTop');
+    scroll.classList.toggle('active', window.scrollY > 500);
+});
+function scrollToTop(params) {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+}
+
+/******************* Scroll to specific section *******************/
+function scrollToSection(id) {
+    document.querySelector(id).scrollIntoView();
+}
+function delayScrollToSection(id) {
+    setTimeout(() => { document.querySelector(id).scrollIntoView(); }, 500);
+    toggleMenu();
+};
+
+// Modifying navbar links action
+document.querySelectorAll(".nav-links")
+    .forEach(link => link.addEventListener("click", () => {
+        event.preventDefault();
+
+        let sectionId = (link.href).substring((link.href).indexOf("#"), (link.href).length);
+
+        scrollToSection(sectionId);
+    })
+    );
+
+document.querySelectorAll(".mobile-nav-links")
+    .forEach(link => link.addEventListener("click", () => {
+        event.preventDefault();
+
+        let sectionId = (link.href).substring((link.href).indexOf("#"), (link.href).length);
+
+        delayScrollToSection(sectionId);
+    })
+    );
+
+/******************* NAVBAR & menu toggle *******************/
+const menuToggle = document.querySelector('.menuToggle');
+const navbarSmallScreen = document.querySelector('.navbar-small-screen');
+const body = document.querySelector('body');
+menuToggle.onclick = function () {
+    menuToggle.classList.toggle('active');
+    navbarSmallScreen.classList.toggle('active');
+    body.classList.toggle('lock-scroll');
+    document.querySelector('header').classList.toggle('active');
+};
+
+function toggleMenu() {
+    menuToggle.classList.remove('active');
+    navbarSmallScreen.classList.remove('active');
+    body.classList.remove('lock-scroll');
+    document.querySelector('header').classList.remove('active');
+}
+
+// delay action for navbar click
+function delay(URL) {
+    setTimeout(function () { window.location = URL }, 500);
+}
